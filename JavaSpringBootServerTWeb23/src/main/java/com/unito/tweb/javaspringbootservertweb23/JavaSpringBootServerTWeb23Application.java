930daeafe74b77f513781e59917ca52269624d8a
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2024-01-05 12:36:39 +0100

    created project

---
+++ JavaSpringBootServerTWeb23/src/main/java/com/unito/tweb/javaspringbootservertweb23/JavaSpringBootServerTWeb23Application.java
@@ -0,0 +1,13 @@
+package com.unito.tweb.javaspringbootservertweb23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootServerTWeb23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootServerTWeb23Application.class, args);
+	}
+
+}
---
+++ JavaSpringBootServerTWeb23/src/test/java/com/unito/tweb/javaspringbootservertweb23/JavaSpringBootServerTWeb23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unito.tweb.javaspringbootservertweb23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootServerTWeb23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
