94c3180de008f3bc5856c4488c740748d29fa248
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2024-01-18 17:37:01 +0100

    Added routes for Games

--- src/main/java/com/unito/tweb/javaspringbootservertweb23/game/GameController.java
+++ src/main/java/com/unito/tweb/javaspringbootservertweb23/game/GameController.java
@@ -1,7 +1,12 @@
 package com.unito.tweb.javaspringbootservertweb23.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GameController {
@@ -10,5 +15,11 @@ public class GameController {
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    @PostMapping("/addGames")
+    public ResponseEntity<String> addGames(@RequestBody List<Game> games){
+        gameService.saveGames(games);
+        return ResponseEntity.ok("Games successfully loaded!");
     }
 }
