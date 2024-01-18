96a1599e1b7883dd7d1595a1ae7dd097980742e6
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2024-01-18 21:13:48 +0100

    Added routes for ClubGames

--- src/main/java/com/unito/tweb/javaspringbootservertweb23/club_game/ClubGame.java
+++ src/main/java/com/unito/tweb/javaspringbootservertweb23/club_game/ClubGame.java
@@ -1,34 +1,51 @@
 package com.unito.tweb.javaspringbootservertweb23.club_game;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 
 @Entity
 @Table(name = "club_games")
 public class ClubGame {
-    @EmbeddedId
-    private ClubGameId clubGameId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "game_id", nullable = false)
+    @JsonProperty("game_id")
+    private Long gameId;
+
+    @Column(name = "club_id", nullable = false)
+    @JsonProperty("club_id")
+    private Long clubId;
 
     @Column(name = "own_goal", nullable = false)
+    @JsonProperty("own_goals")
     private Integer ownGoal;
 
     @Column(name = "own_position", nullable = false)
+    @JsonProperty("own_position")
     private Integer ownPosition;
 
     @Column(name = "own_manager_name")
+    @JsonProperty("own_manager_name")
     private String ownManagerName;
 
     @Column(name = "hosting", nullable = false)
+    @JsonProperty("hosting")
     private boolean hosting;
 
     @Column(name = "is_win", nullable = false)
+    @JsonProperty("is_win")
     private boolean isWin;
 
     @Column(name = "club_formation")
+    @JsonProperty("club_formation")
     private String clubFormation;
 
-    public ClubGame(ClubGameId clubGameId, Integer ownGoal, Integer ownPosition, String ownManagerName, boolean hosting, boolean isWin, String clubFormation) {
-        this.clubGameId = clubGameId;
+    public ClubGame( Long gameId, Long clubId, Integer ownGoal, Integer ownPosition, String ownManagerName, boolean hosting, boolean isWin, String clubFormation) {
+        this.gameId = gameId;
+        this.clubId = clubId;
         this.ownGoal = ownGoal;
         this.ownPosition = ownPosition;
         this.ownManagerName = ownManagerName;
@@ -41,12 +58,20 @@ public class ClubGame {
 
     }
 
-    public ClubGameId getClubGameId() {
-        return clubGameId;
+    public Long getClubId() {
+        return clubId;
     }
 
-    public void setClubGameId(ClubGameId clubGameId) {
-        this.clubGameId = clubGameId;
+    public void setClubId(Long clubId) {
+        this.clubId = clubId;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
     }
 
     public Integer getOwnGoal() {
--- src/main/java/com/unito/tweb/javaspringbootservertweb23/club_game/ClubGameRepository.java
+++ src/main/java/com/unito/tweb/javaspringbootservertweb23/club_game/ClubGameRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClubGameRepository extends JpaRepository<ClubGame, ClubGameId> {
+public interface ClubGameRepository extends JpaRepository<ClubGame, Long> {
 }
