6ac44f8467d7cdedcf9ad0ff1baf5e0bd85512c5
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2024-01-18 21:36:12 +0100

    Added routes for GameEvents

--- src/main/java/com/unito/tweb/javaspringbootservertweb23/game_event/GameEvent.java
+++ src/main/java/com/unito/tweb/javaspringbootservertweb23/game_event/GameEvent.java
@@ -1,43 +1,52 @@
 package com.unito.tweb.javaspringbootservertweb23.game_event;
 
-import com.unito.tweb.javaspringbootservertweb23.game.Game;
-import com.unito.tweb.javaspringbootservertweb23.player.Player;
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "game_events")
 public class GameEvent {
     @Id
     @Column(name = "game_event_id", nullable = false)
+    @JsonProperty("game_event_id")
     private String gameEventId;
 
     @Column(name = "minute", nullable = false)
+    @JsonProperty("minute")
     private Integer minute;
 
     @Column(name = "event_type", nullable = false)
+    @JsonProperty("event_type")
     private String eventType;
 
     @Column(name = "club_id", nullable = false)
+    @JsonProperty("club_id")
     private Long clubId;
 
     @Column(name = "event_description")
+    @JsonProperty("event_description")
     private String eventDescription;
 
     @Column(name = "player_in_id")
+    @JsonProperty("player_in_id")
     private Long playerInId;
 
     @Column(name = "player_assist_id")
+    @JsonProperty("player_assist_id")
     private Long playerAssistId;
 
-    @ManyToOne
-    @JoinColumn(name = "game_id", nullable = false)
-    private Game gameId;
+    @Column(name = "game_id", nullable = false)
+    @JsonProperty("game_id")
+    private Long gameId;
 
-    @ManyToOne
-    @JoinColumn(name = "player_id", nullable = false)
-    private Player playerId;
+    @Column(name = "player_id", nullable = false)
+    @JsonProperty("player_id")
+    private Long playerId;
 
-    public GameEvent(String gameEventId, Integer minute, String eventType, Long clubId, String eventDescription, Long playerInId, Long playerAssistId, Game gameId, Player playerId) {
+    public GameEvent(String gameEventId, Integer minute, String eventType, Long clubId, String eventDescription, Long playerInId, Long playerAssistId, Long gameId, Long playerId) {
         this.gameEventId = gameEventId;
         this.minute = minute;
         this.eventType = eventType;
@@ -109,19 +118,19 @@ public class GameEvent {
         this.playerAssistId = playerAssistId;
     }
 
-    public Game getGameId() {
+    public Long getGameId() {
         return gameId;
     }
 
-    public void setGameId(Game gameId) {
+    public void setGameId(Long gameId) {
         this.gameId = gameId;
     }
 
-    public Player getPlayerId() {
+    public Long getPlayerId() {
         return playerId;
     }
 
-    public void setPlayerId(Player playerId) {
+    public void setPlayerId(Long playerId) {
         this.playerId = playerId;
     }
 }
--- src/main/java/com/unito/tweb/javaspringbootservertweb23/game_event/GameEventController.java
+++ src/main/java/com/unito/tweb/javaspringbootservertweb23/game_event/GameEventController.java
@@ -1,7 +1,12 @@
 package com.unito.tweb.javaspringbootservertweb23.game_event;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GameEventController {
@@ -10,5 +15,11 @@ public class GameEventController {
     @Autowired
     public GameEventController(GameEventService gameEventService) {
         this.gameEventService = gameEventService;
+    }
+
+    @PostMapping("/addGameEvents")
+    public ResponseEntity<String> addGameEvents(@RequestBody List<GameEvent> gameEvents){
+        gameEventService.saveGameEvents(gameEvents);
+        return ResponseEntity.ok("GameEvents successfully loaded!");
     }
 }
